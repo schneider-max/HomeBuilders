05d780f6dac67271bfdb6b3e6eee981bf26f86eb
Sebastian.Kapeller <sebastian.kapeller@students.bs-linz2.ac.at>
2021-10-06 16:09:53 +0200

    setup overnightJs server

--- backend/lib/server.ts
+++ backend/lib/server.ts
@@ -1,7 +1,21 @@
 import app from "./config/app";
+import { Server } from '@overnightjs/core';
+import { Logger } from '@overnightjs/logger';
 
-const PORT = 3000;
 
-app.listen(PORT, () => {
-   console.log('Express server listening on port ' + PORT);
-})
+class DemoServer extends Server {
+
+   private readonly SERVER_START_MSG = 'Demo server started on port: ';
+
+   constructor() {
+      super();
+   }
+
+   public start(port: number): void {
+      this.app.listen(port, () => {
+         Logger.Imp(this.SERVER_START_MSG + port);
+      });
+   }
+}
+
+export default DemoServer;
---
+++ backend/lib/start.ts
@@ -0,0 +1,8 @@
+import DemoServer from './server';
+
+// Start the server or run tests
+if (process.argv[2] !== 'test') {
+    let server = new DemoServer();
+    server.start(3004);
+} else {
+}

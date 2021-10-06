d40c7c4c521c90a549bd8428a5da8b8e61444908
Maximilian Schneider <Maximilian.Schneider@students.bs-linz2.ac.at>
2021-10-06 16:06:52 +0200

    Login start

---
+++ frontend/src/Login/LoginPage.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Button from '@mui/material/Button';
+
+
+function Login() {
+  return (
+    
+        <Button variant="contained">Hello World</Button>
+  
+  );
+}
+
+export default Login;

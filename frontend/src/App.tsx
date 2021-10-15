98b549bc15b18ded8b542ff9d591d5573f5e0ef9
Maximilian Schneider <Maximilian.Schneider@students.bs-linz2.ac.at>
2021-10-15 09:51:13 +0200

    Änderung

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,76 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Box from '@mui/material/Box';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+import Grid from '@mui/material/Grid';
+import Item from '@mui/material/MenuItem'
+
+
+function NavBar() {
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+    setValue(newValue);
+  };
+
+  return (
+    <Box sx={{ width: '100%', bgcolor: 'background.paper'}}>
+      <Tabs value={value} onChange={handleChange} centered>
+        <Tab label="Home" />
+        <Tab label="History" />
+        <Tab label="Logout" />
+      </Tabs>
+    </Box>
+  );
+}
+
+function ProjectList() {
+  return(
+    <Box sx={{height: '600px', borderColor: 'black', margin: '15px'}}>
+      <Grid container spacing={8} sx={{margin: "15px"}}>
+        <Grid item xs={6}>
+          <Item>
+            Projekt 1
+          </Item>
+        </Grid>
+        <Grid item xs={6}>
+          <Item>
+            Projekt 2
+          </Item>
+        </Grid>
+        <Grid item xs>
+          <Item>
+            Projekt 3
+          </Item>
+        </Grid>
+        <Grid item xs>
+          <Item>
+            Projekt 4
+          </Item>
+        </Grid>
+        <Grid item xs>
+          <Item>
+            Projekt 5
+          </Item>
+        </Grid>
+      </Grid>
+    </Box>
+  );
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Box sx={{backgroundColor: 'lightblue', top: '0px', fontSize: '30px', height: '50px', padding: '15px'}} className="titleName">
+        HomeBuilders
+      </Box>
+      <React.StrictMode>
+        <NavBar />
+      </React.StrictMode>  
+      <React.StrictMode>
+        <ProjectList />
+      </React.StrictMode> 
     </div>
   );
 }

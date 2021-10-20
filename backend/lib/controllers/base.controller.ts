b196a36814963685d2586f9be20393963f0a6aad
Strauss-Aigner Manuel <Manuel.Strauss@BSLINZ2-IT.LOCAL>
2021-10-20 14:22:29 +0200

    add logging as middleware

---
+++ backend/lib/controllers/base.controller.ts
@@ -0,0 +1,17 @@
+import { Logger } from "@overnightjs/logger";
+import { NextFunction, Request, Response } from 'express'
+
+
+export class BaseController {
+    protected readonly Ok: number = 200;
+    protected readonly Created: number = 201;
+    protected readonly BadRequest: number = 400;
+    protected readonly Unauthorized: number = 401;
+    protected readonly NotFound: number = 404;
+    protected readonly InternalServerError: number = 500;
+}
+
+export const logger = (req: Request, res: Response, next: NextFunction) => {
+    Logger.Info(`[${req.method}]` + req.originalUrl);
+    next();
+}
--- backend/lib/controllers/customer.controller.ts
+++ backend/lib/controllers/customer.controller.ts
@@ -1,74 +1,74 @@
-import { Controller, Delete, Get, Post, Put } from '@overnightjs/core';
-import { Logger } from '@overnightjs/logger';
-import { Request, Response } from 'express'
+import { Controller, Delete, Get, Middleware, Post, Put } from '@overnightjs/core';
+import { NextFunction, Request, Response } from 'express'
 import { Md5 } from 'ts-md5';
 import { getRepository } from 'typeorm';
 
 import { Customer } from '../db/entities/entity.customer';
 import { ICustomer } from '../modules/interfaces';
+import { BaseController, logger } from './base.controller';
 
 @Controller('api/customers')
-export class CustomerController {
+export class CustomerController extends BaseController {
 
     @Get('')
-    async get(req: Request, res: Response): Promise<any> {
-        Logger.Info(`[${req.method}]` + req.originalUrl);
+    @Middleware([logger])
+    async get(req: Request, res: Response, next: NextFunction): Promise<any> {
         const customers = await getRepository(Customer).find();       
-        return res.status(200).json(this.mapDBOToDTO(customers));
+        return res.status(this.Ok).json(this.mapDBOToDTO(customers));
     }
 
     @Get(':email')
+    @Middleware([logger])
     async getById(req: Request, res: Response): Promise<any> {
-        Logger.Info(`[${req.method}]` + req.originalUrl);
         const customer = await getRepository(Customer).findOne(req.params.email);      
         if (customer != null)
-            return res.status(200).json(this.mapDBOToDTO([customer]));
+            return res.status(this.Ok).json(this.mapDBOToDTO([customer]));
         else
-            return res.status(404).json({});
+            return res.status(this.NotFound).json({});
     }
 
     @Get(':email/:password')
+    @Middleware([logger])
     async getLogin(req: Request, res: Response): Promise<any> {
-        Logger.Info(`[${req.method}]` + req.originalUrl);
         const customer = await getRepository(Customer).findOne(req.params.email);       
         if (customer?.password === Md5.hashStr(req.params.password))
-            return res.status(200).json({});
+            return res.status(this.Ok).json({});
         else
-            return res.status(401).json({});
+            return res.status(this.Unauthorized).json({});
     }
 
     @Post('')
+    @Middleware([logger])    
     async post(req: Request, res: Response): Promise<any> {
-        Logger.Info(`[${req.method}]` + req.originalUrl);
         const customer = getRepository(Customer).create(req.body);
         const results = await getRepository(Customer).save(customer);        
-        return res.status(200).json(this.mapDBOToDTO(results));
+        return res.status(this.Ok).json(this.mapDBOToDTO(results));
     }
 
     @Put('')
+    @Middleware([logger])
     async update(req: Request, res: Response): Promise<any> {
-        Logger.Info(`[${req.method}]` + req.originalUrl);
         const customer = await getRepository(Customer).findOne(req.body.email);   
         if (customer?.password === Md5.hashStr(req.body.password)) {
             getRepository(Customer).merge(customer, req.body);
             const result = await getRepository(Customer).save(customer);
-            return res.status(200).json([result]);
+            return res.status(this.Ok).json([result]);
         }
         else {
-            return res.status(401).json({});
+            return res.status(this.Unauthorized).json({});
         }
     }
 
     @Delete(':email/:password')
+    @Middleware([logger])
     async delete (req: Request, res: Response): Promise<any> {
-        Logger.Info(`[${req.method}]` + req.originalUrl);
         const customer = await getRepository(Customer).findOne(req.params.email);
         if (customer?.password === Md5.hashStr(req.params.password)) {
             const results = await getRepository(Customer).delete(req.params.email);
-            return res.status(200).json(results);
+            return res.status(this.Ok).json(results);
         }
         else
-            return res.status(401).json({});
+            return res.status(this.Unauthorized).json({});
     }
 
     private mapDBOToDTO(customers: Customer[]): ICustomer[] {

671d19c20b2bc969219256872130520a351a4899
Bozin Niklas <Niklas.Bozin@BSLINZ2-IT.LOCAL>
2021-11-03 15:46:54 +0100

    HOM-43-ProjectPage passing props with redirect

--- frontend/src/ProjectPage/ProjectCard.tsx
+++ frontend/src/ProjectPage/ProjectCard.tsx
@@ -20,7 +20,10 @@ export default class ProjectCard extends Component<any> {
   } 
   renderRedirect = () => {
     if (this.state.redirect) {
-      return <Redirect to='/sectors'/>
+      return <Redirect to={{
+        pathname: '/sectors',
+        state: {project: this.props}     
+      }}/>
     }
   }  
 

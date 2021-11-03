ab032d077223c807d6d4418b737bc3b8872de5a0
Maximilian Schneider <Maximilian.Schneider@students.bs-linz2.ac.at>
2021-11-03 08:06:55 +0100

    merge

--- backend/lib/controllers/request.controller.ts
+++ backend/lib/controllers/request.controller.ts
@@ -2,12 +2,14 @@ import { Controller, Get, Middleware } from '@overnightjs/core';
 import { Project } from '../db/entities/entity.project';
 import { Request, Response } from 'express';
 import { getRepository } from 'typeorm';
-import { BaseController, logger } from './base.controller';
+import { BaseController } from './base.controller';
+import { logger } from '../middleware/logger.mw';
+import { authMw } from '../middleware/auth.mw';
 
 @Controller('api/requests')
 export class RequestController extends BaseController {
     @Get(':email')
-    @Middleware([logger])
+    @Middleware([logger, authMw])
     public async get(req: Request, res: Response): Promise<any> {
         const email = req.params.email.toLowerCase();
         const projects = await getRepository(Project)

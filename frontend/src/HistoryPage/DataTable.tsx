01915070cd4e283cf6891157ccd8f1c6e93ed55a
Maximilian Schneider <Maximilian.Schneider@students.bs-linz2.ac.at>
2021-11-08 11:56:39 +0100

    HOM-42-History-Page finished!

--- frontend/src/HistoryPage/DataTable.tsx
+++ frontend/src/HistoryPage/DataTable.tsx
@@ -1,18 +1,20 @@
-import { Backdrop, Box, Fade, Grid, Modal } from '@mui/material';
+import { Backdrop, Box, Fade, Grid, Modal, selectClasses } from '@mui/material';
 import { DataGrid, GridColDef } from '@mui/x-data-grid';
 import React from 'react';
 import { getAxioxInstance } from '../shared/axios';
-// import ChangeProject from './ChangeProjects';
+import { Project } from '../../../backend/lib/db/entities/entity.project'
+
+let projectsAll: Project[];
+let selectedRow: any;
 
 const columns: GridColDef[] = [
-  { field: 'id', headerName: 'Id', flex: 0.5, },
+  { field: 'id', headerName: 'Id', flex: 0.5 },
   { field: 'project', headerName: 'Projekt', flex: 1},
   { field: 'phase', headerName: 'Phase', flex: 1},
   { field: 'status', headerName: 'Status',flex: 1},
   {
     field: 'cost',
     headerName: 'Kosten',
-    //type: 'number',
     flex: 1
   },
   {
@@ -27,12 +29,12 @@ export default class DataTable extends React.Component {
   state = {
     projects: [],
     open: false,
-
   };
  
-  private handleOpen = () => {
+  private handleOpen = (event: any) => {
     this.state.open = true;
-    this.setState(this.state)
+    this.setState(this.state);
+    selectedRow = event.row;
   };
 
   private handleClose = () => {
@@ -40,7 +42,6 @@ export default class DataTable extends React.Component {
     this.setState(this.state);
   };
 
-// set state in this method to trigger re-render on request completion
   componentDidMount() {
       const axios = getAxioxInstance();
 
@@ -53,6 +54,7 @@ export default class DataTable extends React.Component {
   }
 
   render() {
+    projectsAll = this.state.projects;
     return (
       <div style={{ height: 450, width: '100%' }}>
         <DataGrid 
@@ -61,36 +63,35 @@ export default class DataTable extends React.Component {
           pageSize={8}
           rowsPerPageOptions={[8]}
           style={{margin: "8px", backgroundColor: "gray"}}   
-          onRowClick={this.handleOpen} 
+          onRowClick={this.handleOpen}
         />
         <div>
-          <Modal
-            aria-labelledby="transition-modal-title"
-            aria-describedby="transition-modal-description"
-            open={this.state.open}
-            onClose={this.handleClose}
-            closeAfterTransition
-            BackdropComponent={Backdrop}
-            BackdropProps={{
-              timeout: 500,
-            }}
-          >
-            <Fade in={this.state.open}>
-              <Box sx={style}>
-                <Box style={modalHeader}>
-                    Info
-                </Box>
-                <hr></hr>
-                <Information />
+        <Modal
+          aria-labelledby="transition-modal-title"
+          aria-describedby="transition-modal-description"
+          open={this.state.open}
+          onClose={this.handleClose}
+          closeAfterTransition
+          BackdropComponent={Backdrop}
+          BackdropProps={{
+            timeout: 500,
+          }}
+        >
+          <Fade in={this.state.open}>
+            <Box sx={style}>
+              <Box style={modalHeader}>
+                  Info
               </Box>
-            </Fade>
-          </Modal>
+              <hr></hr>
+              <Information />
+            </Box>
+          </Fade>
+        </Modal>
         </div>
       </div>
     );
   }
 }
-
 
 function getValues(projects: any){
   const values : any = [];
@@ -103,11 +104,10 @@ function getValues(projects: any){
   return values;
 }
 
-
 const modalInfo = {
   margin: "2.5px",
   width: "100%",
-  fontSize: "20px",
+  fontSize: "16px",
   fontFamily: "sans-serif",
 }
 
@@ -115,28 +115,27 @@ const modalHeader = {
   marginTop: "15px",
   marginLeft: "0px",
   marginBottom: "15px",
-  fontSize: "32px",
+  fontSize: "24px",
   fontFamily: "sans-serif",
-  // textAlign: "left",
-  // fontsize: "32px",
-  // fontfamily: "sans-serif",
 };
 
 
 const style = {
-position: 'absolute' as 'absolute',
-top: '50%',
-left: '50%',
-transform: 'translate(-50%, -50%)',
-width: 500,
-bgcolor: 'background.paper',
-border: '2px solid #000',
-boxShadow: 24,
-p: 4,
+  position: 'absolute' as 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 500,
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  p: 4,
 };
 
-function Info(project: any) {
-  const request = project.request;
+function Info() {
+  const project_id = getProject();
+ 
+  const request = projectsAll[project_id - 1].requests[selectedRow.id - 1]
 
   return (
       <Box>
@@ -145,7 +144,7 @@ function Info(project: any) {
                   Email:
               </Grid>
               <Grid item xs={9}>
-                  testus.test@schule.at
+                  {request.email}
               </Grid>
           </Grid>
           <Grid style={modalInfo} container spacing={3}>
@@ -153,7 +152,7 @@ function Info(project: any) {
                   Betreff:
               </Grid>
               <Grid item xs={9}>
-                  Test
+                 {request.subject}
               </Grid>
           </Grid>
           <Grid style={modalInfo} container spacing={3}>
@@ -161,46 +160,39 @@ function Info(project: any) {
                   Budget:
               </Grid>
               <Grid item xs={9}>
-                  3000000000000 €
+                {request.budget}
               </Grid>
           </Grid>
           <Grid style={modalInfo} container spacing={3}>
               <Grid item xs={3}>
-                  Notizen:
+                  Nachricht:
               </Grid>
               <Grid item xs={9}>
-                  Das ist eine Testnotiz über die Kommunikation mit Herr Testus Test!
+                  {request.message}
               </Grid>
           </Grid>
       </Box>
   )
 }
 
+function getProject() : number {
+
+  for (let index = 0; index < projectsAll.length; index++) {
+    const project = projectsAll[index];
+    let projectName : string = project.name;
+    let selectedrowName : string = selectedRow.project;
+    if (projectName === selectedrowName) {
+      return project.id;
+    }
+  }
+  return -1;
+}
+
 class Information extends React.Component {
-  constructor(props : any){
-      super(props);
-
-  }
-  state = {
-      projects: []
-  };
-
-  // set state in this method to trigger re-render on request completion
-  componentDidMount() {
-      const axios = require("axios").create({
-          baseURL: 'http://localhost:3001'
-      });
-
-      axios.get("/api/projects/TestUser@hotmail.com")
-          .then(res => {
-              const projects = res.data;
-              this.setState({ projects });
-          })
-  }
-
   render(){
+  
       return(
-          <Info {... this.state.projects} />
+          <Info  />
       )
   }
 }

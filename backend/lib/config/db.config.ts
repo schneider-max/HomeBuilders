ea5094b8acdbcb24c5c7c3ca38645029a35c5ed9
Sebastian.Kapeller <sebastian.kapeller@students.bs-linz2.ac.at>
2021-11-09 09:22:43 +0100

    set db config back to initial settings

--- backend/lib/config/db.config.ts
+++ backend/lib/config/db.config.ts
@@ -1,11 +1,11 @@
-import { DBLogger } from '../db/maintenance/db.logger';
+import {DBLogger} from '../db/maintenance/db.logger';
 
 module.exports = {
     type: 'mysql',
     host: 'localhost',
     port: 3306,
-    username: 'HomeBuildersTest',
-    password: 'Passwort12',
+    username: 'root',
+    password: 'password',
     database: 'homebuildersdb',
     synchronize: true,
     logging: new DBLogger(),

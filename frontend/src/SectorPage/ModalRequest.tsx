68e5c5820afdad0aa16bacdceb9c0da8d17cb447
Sebastian.Kapeller <sebastian.kapeller@students.bs-linz2.ac.at>
2021-11-10 13:59:05 +0100

    lint error fix, clean up

--- frontend/src/SectorPage/ModalRequest.tsx
+++ frontend/src/SectorPage/ModalRequest.tsx
@@ -5,6 +5,7 @@ import Typography from '@mui/material/Typography';
 import Modal from '@mui/material/Modal';
 import TextField from '@mui/material/TextField';
 import axios, {AxiosRequestConfig} from "axios";
+import {Alert} from "@mui/material";
 
 const style = {
     position: 'absolute' as 'absolute',
@@ -21,47 +22,95 @@ const styleInputs = {
     marginBottom: "2em",
 };
 
-function handleRequestSent(event: any, project, supplier, sector) {
-    event.preventDefault();
-
-    const data = new FormData(event.currentTarget);
-
-    try {
-        let sessionToken = sessionStorage.getItem('token');
-        if (sessionToken != null && data != null) {
-            const options: AxiosRequestConfig = {
-                url: "http://localhost:3001/api/requests",
-                method: 'POST',
-                headers: {
-                    'Accept': 'application/json',
-                    'Content-Type': 'application/json',
-                    "X-JWT-Token": sessionToken
-                },
-                data: {
-                    firstname: data.get("firstname"),
-                    lastname: data.get("lastname"),
-                    email: data.get("email"),
-                    budget: Number(data.get("budget")),
-                    subject: data.get("subject"),
-                    message: data.get("msg"),
-                    sectors: sector,
-                    suppliers: supplier,
-                    projects: project
-                }
-            }
-            axios(options)
-                .then(res => console.log(res))
-                .catch(ex => console.log(ex));
-        }
-    } catch (ex: any) {
-
-    }
-}
-
 export default function ModalRequest(props: any) {
+
+    const [requestErrorAlert, setRequestErrorAlert] = React.useState(false);
+    const [requestErrorAlertMsg, setRequestErrorAlertMsg] = React.useState('');
+    const [requestSuccessAlert, setRequestSuccessAlert] = React.useState(false);
+    const [requestSuccessAlertMsg, setRequestSuccessAlertMsg] = React.useState('');
     const [open, setOpen] = React.useState(false);
+
     const handleOpen = () => setOpen(true);
-    const handleClose = () => setOpen(false);
+    const handleClose = () => {
+        setOpen(false);
+        resetAlerts();
+    }
+
+    function handleRequestError(ex) {
+        setRequestErrorAlert(true);
+        setRequestErrorAlertMsg('Request proccessing faild due to following error: ' + ex);
+
+        setRequestSuccessAlert(false);
+        setRequestSuccessAlertMsg('');
+    }
+
+    function handleRequestSuccess() {
+        setRequestErrorAlert(false);
+        setRequestErrorAlertMsg('');
+
+        setRequestSuccessAlert(true);
+        setRequestSuccessAlertMsg("Request was successfully sent to Businesspartner");
+    }
+
+    function resetAlerts() {
+        setRequestErrorAlert(false);
+        setRequestErrorAlertMsg('');
+
+        setRequestSuccessAlert(false);
+        setRequestSuccessAlertMsg('');
+    }
+
+    function handleRequestSent(event: any, project, supplier, sector) {
+        event.preventDefault();
+
+        const data = new FormData(event.currentTarget);
+
+        let firstname = data.get("firstname");
+        let lastname = data.get("lastname");
+        let email = data.get("email");
+        let budget = Number(data.get("budget"));
+        let subject = data.get("subject");
+        let msg = data.get("msg");
+
+        if (firstname === '' || lastname === '' || email === '' || subject === '') {
+            handleRequestError("Not all mandatory Fields where filled out!")
+        } else {
+            try {
+                let sessionToken = sessionStorage.getItem('token');
+                if (sessionToken != null) {
+                    const options: AxiosRequestConfig = {
+                        url: "http://localhost:3001/api/requests",
+                        method: 'POST',
+                        headers: {
+                            'Accept': 'application/json',
+                            'Content-Type': 'application/json',
+                            "X-JWT-Token": sessionToken
+                        },
+                        data: {
+                            firstname: firstname,
+                            lastname: lastname,
+                            email: email,
+                            budget: budget,
+                            subject: subject,
+                            message: msg,
+                            sectors: sector,
+                            suppliers: supplier,
+                            projects: project
+                        }
+                    }
+                    axios(options)
+                        .then(() => {
+                            handleRequestSuccess();
+                        })
+                        .catch((ex) => {
+                            handleRequestError(ex);
+                        });
+                }
+            } catch (ex: any) {
+                handleRequestError(ex)
+            }
+        }
+    }
 
     return (
         <div>
@@ -76,26 +125,53 @@ export default function ModalRequest(props: any) {
                     <Typography id="modal-modal-title" variant="h6" component="h2">
                         Request to {props.supplier.companyName}
                     </Typography>
+
+                    <div id={"RequestErrorAlert"}>
+                        {requestErrorAlert ? <Alert
+                            severity='error'
+                            onClick={() => {
+                                setRequestErrorAlert(false);
+                                setRequestErrorAlertMsg('');
+                                return null;
+                            }}
+                        >{requestErrorAlertMsg}
+                        </Alert> : <></>}
+                    </div>
+                    <div id={"requestSuccessAlert"}>
+                        {requestSuccessAlert ? <Alert
+                            severity='success'
+                            onClick={() => {
+                                setRequestSuccessAlert(false);
+                                setRequestSuccessAlertMsg('');
+                                return null;
+                            }}
+                        >{requestSuccessAlertMsg}
+                        </Alert> : <></>}
+                    </div>
+
                     <Typography id="modal-modal-description" sx={{mt: 2}}>
                         <div>
                             <form className="mui-form" onSubmit={(event: any) => {
                                 handleRequestSent(event, props.project, props.supplier, props.sector);
-                                handleClose()
                             }}>
-                                <TextField sx={styleInputs} fullWidth label="First name" name="firstname">
+                                <TextField sx={styleInputs} fullWidth label="First name" name="firstname" type={"text"}
+                                           required={true}>
                                     <input type="text"/>
                                 </TextField>
-                                <TextField sx={styleInputs} fullWidth label="Last name" name="lastname">
+                                <TextField sx={styleInputs} fullWidth label="Last name" name="lastname" type={"text"}
+                                           required={true}>
                                     <input type="text"/>
                                 </TextField>
                                 <TextField sx={styleInputs} fullWidth label="E-Mail" name="email"
-                                           defaultValue={sessionStorage.getItem("email")?.toString()!}>
+                                           defaultValue={sessionStorage.getItem("email")?.toString()!}
+                                           type={"email"} required={true}>
                                     <input type="email"/>
                                 </TextField>
-                                <TextField sx={styleInputs} fullWidth label="Budget" name="budget">
+                                <TextField sx={styleInputs} fullWidth label="Budget" name="budget" type={"number"}>
                                     <input type="number"/>
                                 </TextField>
-                                <TextField sx={styleInputs} fullWidth label="Subject" name="subject">
+                                <TextField sx={styleInputs} fullWidth label="Subject" name="subject"
+                                           type={"text"} required={true}>
                                     <input type="text"/>
                                 </TextField>
                                 <TextField sx={styleInputs} fullWidth label="Message / Notes" name="msg"
